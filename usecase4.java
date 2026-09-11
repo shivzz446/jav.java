3d0c19547692d7174068293e91eba4db50658336
shivzz446 <shivanshthakur2ooo6@gmail.com>
2026-09-11 15:10:34 +0530

    Add files via upload

---
+++ usecase4.java
@@ -0,0 +1,38 @@
+public class usecase4 {
+
+    public static void main(String[] args) {
+
+        double balance = 5000.0;
+        double withdrawAmount = 7500.0;
+
+        try {
+            withdraw(balance, withdrawAmount);
+        }
+        catch (InsufficientBalanceException e) {
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("Transaction attempt completed.");
+        }
+    }
+
+    static void withdraw(double balance, double amount)
+            throws InsufficientBalanceException {
+
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance.");
+        }
+        else {
+            System.out.println(
+                "Withdrawal successful. New balance: " + (balance - amount)
+            );
+        }
+    }
+}
+
+class InsufficientBalanceException extends Exception {
+
+    InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
